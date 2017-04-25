1b35049e3f3ef8dbc09e2dea829e1cd575ca9c7c
Kar Yin Ng <karyin3@uw.edu>
2017-04-25 16:55:06 -0700

    Finish bluetooth labwork

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -20,6 +20,7 @@ import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -43,6 +44,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.common.logger.Log;
+
+import static android.bluetooth.BluetoothAdapter.getDefaultAdapter;
 
 /**
  * This fragment controls Bluetooth to communicate with other devices.
@@ -92,12 +95,17 @@ public class BluetoothChatFragment extends Fragment {
         setHasOptionsMenu(true);
 
         // TODO: Get local Bluetooth adapter
+        BluetoothAdapter mBluetoothAdapter = getDefaultAdapter();
 
         /**
          *  TODO: If the adapter is null, then Bluetooth is not supported.
          *  TODO: Let us know via Toast Bluetooth is not available.
          *  TODO: Then finish() the fragment's attached Activity.
          */
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getActivity().getApplicationContext(),"Bluetooth is not available", Toast.LENGTH_LONG).show();
+            getActivity().finish();
+        }
     }
 
 
@@ -105,7 +113,7 @@ public class BluetoothChatFragment extends Fragment {
     public void onStart() {
         super.onStart();
         if (mBluetoothAdapter != null) {
-            // If BT is not on, request that it be enabled.
+            // If BT is not on, request that it ibe enabled.
             // setupChat() will then be called during onActivityResult
 
             if(!mBluetoothAdapter.isEnabled()) {
@@ -114,6 +122,8 @@ public class BluetoothChatFragment extends Fragment {
                  * TODO: If adapter is NOT enabled, send an Implicit Intent with the action ACTION_REQUEST_ENABLE (send for a result)
                  * TODO: Otherwise, check if the BluetoothChatService is null, and if so call setupChat() to set it up.
                  */
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
 
             }
             else if(mChatService == null){
@@ -204,6 +214,14 @@ public class BluetoothChatFragment extends Fragment {
          * TODO: If not, send an Implicit Intent for the ACTION_REQUEST_DISCOVERABLE action (with an extra for the EXTRA_DISCOVERABLE_DURATION)
          */
 
+        if (mBluetoothAdapter != null) {
+            if(mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(intent);
+            }
+        }
+
     }
 
     /**
@@ -227,6 +245,10 @@ public class BluetoothChatFragment extends Fragment {
              * TODO: set the mOutStringBuffer's length to 0 (since we've cleared the message
              * TODO: set the mOutEditText's text to be the (now empty) mOutStringBuffer
              */
+            byte[] byteMessage = message.getBytes();
+            mChatService.write(byteMessage);
+            mOutStringBuffer.setLength(0);
+            mOutEditText.setText(mOutStringBuffer);
 
         }
     }
@@ -376,6 +398,11 @@ public class BluetoothChatFragment extends Fragment {
          * TODO: Then use the adapter's .getRemoteDevice() to get a reference to the device
          * TODO: Then tell the mChatService to connect to that device!
          */
+        Bundle bundle = getArguments();
+        String deviceAddress = bundle.getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(deviceAddress);
+        mChatService.connect(device, secure);
 
     }
 
